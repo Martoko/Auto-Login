a32fc1bfb04bf59c02261e25d017f3ad0c0ea682
Martoko <Martoko@users.noreply.github.com>
2017-02-13 12:33:59 +0100

    removed superflous files

--- login_page.js
+++
@@ -1,8 +0,0 @@
-function ITU_WAYF_LoginPage() {
-  function displayMessageToUser(message, color) {}
-
-  function getUsernameInput() {}
-  function getPasswordInput() {}
-  function getForm() {}
-  function hasError() {}
-}
